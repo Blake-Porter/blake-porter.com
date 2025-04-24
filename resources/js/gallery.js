cd851d7dbc3dc2ed95871818a1dce74d489bb56e
Blake-Porter <Bporter273@gmail.com>
2025-04-24 14:54:43 -0700

    Update gallery.js

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const navButtons = document.querySelectorAll('.gallery-nav button');
     const regionsContainer = document.querySelector('.regions-container');
+    const regions = document.querySelectorAll('.region');
   
     navButtons.forEach((btn, idx) => {
       btn.addEventListener('click', () => {
+        // 1. Button active state
         document.querySelector('.gallery-nav button.active').classList.remove('active');
         btn.classList.add('active');
+  
+        // 2. Section active state
+        document.querySelector('.region.active').classList.remove('active');
+        regions[idx].classList.add('active');
+  
+        // 3. Slide
         regionsContainer.style.transform = `translateX(-${idx * 100}%)`;
       });
     });
