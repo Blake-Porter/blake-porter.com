74d58d32cd13daf493937c60bf61774a9b49d967
Blake-Porter <Bporter273@gmail.com>
2025-04-25 11:50:20 -0700

    will it work?

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,19 @@
 module.exports = function(eleventyConfig) {
-  // 1) Copy static assets
   eleventyConfig.addPassthroughCopy("resources");
 
-  // 2) Build a "guides" collection from all Markdown under content/guides/
   eleventyConfig.addCollection("guides", collectionApi => {
-    return collectionApi.getFilteredByGlob("content/guides/**/*.md").reverse();
+    return collectionApi
+      .getFilteredByGlob("content/*/*.md")  // catch both features & itineraries
+      .reverse();
   });
 
-  // 3) Ignore your templates folder so Eleventy never tries to build skeleton.md
   eleventyConfig.ignores.add("_templates");
 
-  // 4) Return your directory structure
   return {
     dir: {
-      input: ".",            // project root (so it sees guides.html, content/, etc.)
-      includes: "_includes", // your Eleventy layouts/partials
-      output: "_site"        // built site goes here
+      input: ".",             // project root
+      includes: "_includes",
+      output: "_site"
     }
   };
-};
+}
